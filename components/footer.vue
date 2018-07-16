ad9b17d024661fc3a0f7ace25b85dad7f682b09c
admiralkot <37459208+admiralkot@users.noreply.github.com>
2018-07-17 01:31:07 +0500

    test_1
    привет

---
+++ components/footer.vue
@@ -0,0 +1,38 @@
+<template>
+
+<footer class="footer" id="footer-site">
+  <hr/>
+    <div class="row justify-content-center " >
+      <i class="fas fa-code fa-4x" style="margin-right: 20px;"></i>
+      <div>
+        <h5>© 2017 - {{new Date().getUTCFullYear()}}</h5>
+      </div>
+    </div>
+  </footer>
+
+</template>
+
+<script>
+export default {
+  name:'myFooter',
+  data: ()=>({
+    date: null,
+
+  }),
+}
+</script>
+
+<style>
+html, body {
+    margin: 5px;
+    padding: 0;
+}
+
+#footer-site{
+  padding-bottom: 20px;
+  padding-top: 20px;
+  text-align: center;
+
+}
+
+</style>
---
+++ layouts/default.vue
@@ -0,0 +1,13 @@
+<template>
+  <section>  
+    <nuxt/>
+    <my-footer/>
+  </section>
+</template>
+
+<script>
+  import myFooter from '~/components/footer.vue'
+  export default {
+    components: { myFooter},
+  }
+</script>
